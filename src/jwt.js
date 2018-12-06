eb15ee0f9dd6f47a3b8d8d90dd80dc2e434d8da6
Fred Lackey <fred.lackey@gmail.com>
2018-12-05 23:26:06 -0500

    jwt verify exp ignored

--- src/jwt.js
+++ src/jwt.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 
-const decode = (token, secret) => {
+const decode = (token, secret, ignoreExpiration = true) => {
   try {
-    const result = jwt.verify(token, secret);
+    const result = jwt.verify(token, secret, { ignoreExpiration });
     return result;
   } catch (er) {
     return undefined;
