d6537211a0483d838d90df708aa8d0e0a0029efe
Fred Lackey <fred.lackey@gmail.com>
2019-02-17 10:36:12 -0500

    v0.3.5

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cleaner-node
 Helpful utilities and scripts to make Node projects more legible and easier for the next developer to take over.
+
+## Changes
+
+|  Version  |  Date  |  Description  |
+|-----------|--------|---------------|
+|  0.3.5  |  2019/02/17  |  Add `objects.getValue`, `getValues`, and `setValue` to get & set properties of an object using fully qualified path names.  |
--- src/objects.js
+++ src/objects.js
@@ -1,4 +1,7 @@
-const strings = require('./strings');
+const { isValid: isValidString } = require('./strings');
+const { count } = require('./arrays');
+
+const isValid = value => (typeof value === 'object' && !(value instanceof Array));
 
 const getId = item => {
   if (typeof item === 'object' && item instanceof Array) { return undefined; }
@@ -74,7 +77,7 @@ const toDto = value => {
   if (typeof value === 'undefined') { return value; }
   if (value instanceof Array) { return value; }
   const copy = JSON.parse(JSON.stringify(value));
-  const all = Object.keys(copy).filter(strings.isValid);
+  const all = Object.keys(copy).filter(isValidString);
   // eslint-disable-next-line no-eq-null
   const toRemove = all.filter(k => (copy[k] == null));
   if (toRemove.includes('id') && !toRemove.includes('uid') && Object.keys(copy).includes('uid')) {
@@ -91,14 +94,63 @@ const toDtos = values => {
     .map(v => (toDto(v)));
 }
 
+// --- getValue ... from singular property
+
+function getValue (item, keyOrPath) {
+  const keys = keyOrPath.split('.');
+  const used = [];
+  while (keys.length > 0) {
+    const key = keys.shift();
+    used.push(key);
+    if (!isValid(item[key])) {
+      throw new Error(`path ${used.join('.')} is not an object`);
+    }
+    item = item[key];
+  }
+  return item[keys[0]];
+}
+function getValues (items, keyOrPath, allowDuplicates) {
+  const result = [];
+  [].concat(items).filter(isValid).forEach(item => {
+    const value = getValue(item, keyOrPath);
+    if (allowDuplicates || !result.includes(value)) {
+      result.push(value);
+    }
+  })
+  return result;
+}
+
+// --- setValue
+
+function setValue (obj = {}, path, value) {
+  const keys = path.split('.');
+  const used = [];
+  while (keys.length > 1) {
+    const key = keys.shift();
+    used.push(key);
+    if (!(key in obj)) {
+      obj[key] = {};
+    } else if (!isValid(obj[key])) {
+      throw new Error(`path ${used.join('.')} is set to a primative value`);
+    }
+    obj = obj[key];
+  }
+  obj[keys[0]] = value;
+  return obj;
+}
+
 module.exports = {
   findOne,
   getId,
   getIds,
   getUid,
   getUids,
+  getValue,
+  getValues,
   isDefined,
+  isValid,
   notDefined,
+  setValue,
   toDto,
   toDtos
 };
