ec0ac7b9d4c812318d114921377a5d3afca24230
Fred Lackey <fred.lackey@gmail.com>
2025-04-24 07:20:23 -0400

    update newCode

--- src/utils/new-code.js
+++ src/utils/new-code.js
@@ -1,12 +1,15 @@
 const crypto = require('crypto');
-const c = require('../constants');
+const c      = require('../constants');
 
 const newCode = (totalLength = c.DEFAULTS.CODE.LENGTH, chars = c.DEFAULTS.CODE.CHARS) => {
-  const rnd = crypto.randomBytes(totalLength);
-  const value = new Array(totalLength);
-  const len = chars.length;
 
-  for (let i = 0; i < totalLength; i += 1) {
+  const _length = Number(totalLength);
+
+  const rnd   = crypto.randomBytes(_length);
+  const value = new Array(_length);
+  const len   = chars.length;
+
+  for (let i = 0; i < _length; i += 1) {
     value[i] = chars[rnd[i] % len];
   }
 
